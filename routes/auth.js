e5411a680215a8be65e62bb0698bf16bea02b196
adamsyy <adamoommenjacob@gmail.com>
2022-11-20 19:46:35 +0530

    ss

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const { signup } = require("../controller/auth/signup")
 const { login } = require("../controller/auth/login")
+const { user } = require("../controller/auth/user")
 
 const {add_healthscore}=require('../controller/healthscore')
 const {food}=require('../controller/food')
@@ -8,6 +9,7 @@ const router = require("express").Router()
 
 router.post("/signup", signup)
 router.post("/login", login)
+router.get("/user", user)
 
 
 router.post("/add_healthscore",add_healthscore)
